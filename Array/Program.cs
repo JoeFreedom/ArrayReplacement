29c12282ead55c6ee7ceed3622cb30625fdc493e
JoeFreedom <39825386+JoeFreedom@users.noreply.github.com>
2020-04-16 16:58:42 +0300

    Added another class work to master

---
+++ Array/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using static System.Console;
+
+namespace ConsoleArrayElementsEqualZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Найти в массиве всё элементы равные нулю.
+            // Скопировать эти элементы в другой массив
+
+            const int SIZE = 6;
+            int[] arrayElements = new int[SIZE]{0, 5, 0, 5, 3, 0};
+            int[] zeroElements = new int[SIZE];
+
+           /*Random random = new Random();
+            for (int i = 0; i < SIZE; i++)
+            {
+                arrayElements[i] = random.Next(0, 0);
+            }*/
+
+            int zero = 0;
+            for (int i = 0; i < SIZE; i++)
+            {
+                if(arrayElements[i] == 0)
+                {
+                    zeroElements[zero] = arrayElements[i];
+                    zero++;
+                }
+               
+            }
+            for (int i = 0; i < zero; i++)
+            {
+                Write($"{zeroElements[i]} \t");
+            }
+
+            WriteLine("\n");
+            WriteLine($"There are {zero} zero(s) elements in the array");
+            ReadKey();
+        }
+    }
+}
